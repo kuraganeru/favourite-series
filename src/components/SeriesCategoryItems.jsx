fa511dde5553d31d50df99721c05c7dad3add898
andy <n.andyheng@gmail.com>
2023-05-18 19:48:30 -0400

    update parent container to div

--- src/components/SeriesCategoryItems.jsx
+++ src/components/SeriesCategoryItems.jsx
@@ -1,11 +1,12 @@
 import SeriesItem from "./SeriesItem"
+import "./SeriesCategoryItems.css"
 
 export default function SeriesCategoryItems({series}) {
     return (
-        <>
+        <div className="flex">
             {series.map(oneSeries => (
                 <SeriesItem oneSeries={oneSeries} key={oneSeries.title} />
             ))}
-        </>
+        </div>
     )
 }
