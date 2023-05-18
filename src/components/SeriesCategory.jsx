8716cba63fe06b89568d4f7c3e49a717a7e42bf8
andy <n.andyheng@gmail.com>
2023-05-18 19:44:09 -0400

    update parent container to div

--- src/components/SeriesCategory.jsx
+++ src/components/SeriesCategory.jsx
@@ -1,11 +1,12 @@
 import SeriesIndex from "./SeriesIndex"
 import SeriesCategoryItems from "./SeriesCategoryItems"
+import "./SeriesCategory.css"
 
 export default function SeriesCategory({series}) {
     return (
-        <>
+        <div className="series-category">
             <SeriesCategoryItems series={series} />
             <SeriesIndex series={series} />
-        </>
+        </div>
     )
 }
