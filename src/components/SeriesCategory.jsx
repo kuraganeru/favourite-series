72327ff1b61a201debf45ddc61d7e8efb4142fdb
andy <n.andyheng@gmail.com>
2023-05-20 12:37:43 -0400

    add clickedSeries and onSetClickedSeriesChange props to SeriesCategory

--- src/components/SeriesCategory.jsx
+++ src/components/SeriesCategory.jsx
@@ -2,11 +2,19 @@ import SeriesIndex from "./SeriesIndex"
 import SeriesCategoryItems from "./SeriesCategoryItems"
 import "./SeriesCategory.css"
 
-export default function SeriesCategory({series}) {
+export default function SeriesCategory({series, clickedSeries, handleSetClickedSeries}) {
     return (
         <div className="series-category">
-            <SeriesCategoryItems series={series} />
-            <SeriesIndex series={series} />
+            <SeriesCategoryItems 
+                series={series} 
+                clickedSeries={clickedSeries} 
+                onSetClickedSeriesChange={handleSetClickedSeries} 
+            />
+            <SeriesIndex 
+                series={series} 
+                onSetClickedSeriesChange={handleSetClickedSeries} 
+                clickedSeries={clickedSeries} 
+            />
         </div>
     )
 }
