f1f685d0fe6d07d4804fbf5764cb205619211273
andy <n.andyheng@gmail.com>
2023-05-20 14:11:23 -0400

    whitespace

--- src/FavouriteSeriesGrid.jsx
+++ src/FavouriteSeriesGrid.jsx
@@ -9,29 +9,29 @@ function App() {
   const [clickedSeries, setClickedSeries] = useState(null)
   const [searchText, setSearchText] = useState("")
 
-  const series = [{title: "Devil May Cry 3", image: "https://i.imgur.com/lBZNKyA.jpg", id: 1}, {title: "Dead Rising", image: "https://i.imgur.com/n2MJr5O.jpg", id: 2}, {title: "Nioh 2", image: "https://i0.wp.com/tryrolling.com/wp-content/uploads/2020/04/Nioh-2-Background.jpg?resize=150%2C150&ssl=1", id: 3}]
+  const series = [{ title: "Devil May Cry 3", image: "https://i.imgur.com/lBZNKyA.jpg", id: 1 }, { title: "Dead Rising", image: "https://i.imgur.com/n2MJr5O.jpg", id: 2 }, { title: "Nioh 2", image: "https://i0.wp.com/tryrolling.com/wp-content/uploads/2020/04/Nioh-2-Background.jpg?resize=150%2C150&ssl=1", id: 3 }]
 
   function handleSetClickedSeries(oneSeries, originalElement) {
-    setClickedSeries({...oneSeries, originalElement})
-}
+    setClickedSeries({ ...oneSeries, originalElement })
+  }
 
   return (
     <>
-      <SeriesCategoryHeader 
-        categoryName={categoryName} 
-        editCategoryName={editCategoryName} 
+      <SeriesCategoryHeader
+        categoryName={categoryName}
+        editCategoryName={editCategoryName}
         onSetEditCategoryNameChange={setEditCategoryName}
         onSetCategoryNameChange={setCategoryName}
-        />
+      />
       <AddSeriesSearch
         searchText={searchText}
         onSetSearchText={setSearchText}
       />
-      <SeriesCategory 
-        series={series} 
+      <SeriesCategory
+        series={series}
         clickedSeries={clickedSeries}
         handleSetClickedSeries={handleSetClickedSeries}
-        />
+      />
     </>
   )
 }
