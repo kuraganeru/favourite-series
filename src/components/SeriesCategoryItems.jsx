3fbe9aa20a7cc7461502cc163ff98ff7f765caf9
andy <n.andyheng@gmail.com>
2023-05-20 12:36:39 -0400

    add clickedSeries and onSetClickedSeriesChange props to SeriesItem

--- src/components/SeriesCategoryItems.jsx
+++ src/components/SeriesCategoryItems.jsx
@@ -1,11 +1,16 @@
 import SeriesItem from "./SeriesItem"
 import "./SeriesCategoryItems.css"
 
-export default function SeriesCategoryItems({series}) {
+export default function SeriesCategoryItems({series, clickedSeries, onSetClickedSeriesChange}) {
     return (
         <div className="flex">
             {series.map(oneSeries => (
-                <SeriesItem oneSeries={oneSeries} key={oneSeries.title} />
+                <SeriesItem 
+                    oneSeries={oneSeries} 
+                    key={oneSeries.title} 
+                    clickedSeries={clickedSeries}
+                    onSetClickedSeriesChange={onSetClickedSeriesChange}
+                />
             ))}
         </div>
     )
