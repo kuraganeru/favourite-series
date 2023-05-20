263de4c8ff313747cdd8329c2d03c5ebcbfe021e
andy <n.andyheng@gmail.com>
2023-05-20 12:35:03 -0400

    add event listeners and conditional className for SeriesItem

--- src/components/SeriesItem.jsx
+++ src/components/SeriesItem.jsx
@@ -1,7 +1,15 @@
-export default function SeriesItem({ oneSeries }) {
+import "./SeriesItem.css"
+
+export default function SeriesItem({ oneSeries, clickedSeries, onSetClickedSeriesChange }) {
     return (
         <div key={oneSeries.id}>
-            <img src={oneSeries.image} alt={oneSeries.title} />
+            <img 
+                className={`image ${clickedSeries?.originalElement !== "img" && clickedSeries?.id === oneSeries?.id && "img-selected"}`} 
+                src={oneSeries.image} 
+                alt={oneSeries.title}
+                onMouseEnter={(ev) => onSetClickedSeriesChange(oneSeries, "img")}
+                onMouseLeave={() => onSetClickedSeriesChange(null)}
+            />
         </div>
     )
 }
