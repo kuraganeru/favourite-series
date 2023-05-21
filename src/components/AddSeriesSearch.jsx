c8ac2cd08cdd1c6a26ef45cdc73743b5d55dcbec
andy <n.andyheng@gmail.com>
2023-05-21 10:27:38 -0400

    add enter keypress event to search input

--- src/components/AddSeriesSearch.jsx
+++ src/components/AddSeriesSearch.jsx
@@ -1,4 +1,9 @@
 export default function AddSeriesSearch({ searchText, onSetSearchText, handleFetchData, requestLoading }) {
+    function handleOnKeyDown(e) {
+        if (e.key === "Enter") {
+            handleFetchData()
+        }
+    }
     return (
         <>
             <h2>Add Series</h2>
@@ -6,6 +11,7 @@ export default function AddSeriesSearch({ searchText, onSetSearchText, handleFet
                 type="text"
                 value={searchText}
                 onChange={(e) => onSetSearchText(e.target.value)}
+                onKeyDown={(e) => handleOnKeyDown(e)}
             />
             <button onClick={handleFetchData}>Click Me</button>
             <p>{requestLoading ? "Loading..." : "Waiting for request"}</p>
