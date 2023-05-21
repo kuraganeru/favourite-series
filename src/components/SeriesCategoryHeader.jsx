254fc53964e96f43af7c0a94a2bf8adbcd604bfa
andy <n.andyheng@gmail.com>
2023-05-21 13:10:30 -0400

    use pico css container for header

--- src/components/SeriesCategoryHeader.jsx
+++ src/components/SeriesCategoryHeader.jsx
@@ -3,13 +3,11 @@ import "./css/SeriesCategoryHeader.css"
 
 export default function SeriesCategoryHeader({ categoryName, editCategoryName, onSetEditCategoryNameChange, onSetCategoryNameChange }) {
     return (
-        <header className="category-header">
-            <div className="header-title">
-                {editCategoryName ?
-                    <CategoryEdit categoryName={categoryName} onSetCategoryNameChange={onSetCategoryNameChange} /> :
-                    <h2>{categoryName}</h2>
-                }
-            </div>
+        <header className="container">
+            {editCategoryName ?
+                <CategoryEdit categoryName={categoryName} onSetCategoryNameChange={onSetCategoryNameChange} /> :
+                <h2>{categoryName}</h2>
+            }
             <button onClick={() => onSetEditCategoryNameChange(!editCategoryName)}>
                 {editCategoryName ? "Save" : "Edit"}
             </button>
