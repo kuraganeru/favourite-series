1271ff1af2161000fc6bd68dd769deb7e153662d
andy <n.andyheng@gmail.com>
2023-05-21 13:11:32 -0400

    remove parent container, and update conditional style on image

--- src/components/SeriesItem.jsx
+++ src/components/SeriesItem.jsx
@@ -2,14 +2,14 @@ import "./SeriesItem.css"
 
 export default function SeriesItem({ oneSeries, clickedSeries, onSetClickedSeriesChange }) {
     return (
-        <div>
+        <>
             <img 
-                className={`image ${clickedSeries?.originalElement !== "img" && clickedSeries?.id === oneSeries?.id && "img-selected"}`} 
+                className={`image ${clickedSeries?.originalElement !== "img" && clickedSeries?.id === oneSeries?.id ? "img-selected" : ""}`} 
                 src={`https://images.igdb.com/igdb/image/upload/t_thumb_2x/${oneSeries.cover.image_id}.jpg`} 
                 alt={oneSeries.title}
                 onMouseEnter={() => onSetClickedSeriesChange(oneSeries, "img")}
                 onMouseLeave={() => onSetClickedSeriesChange(null)}
             />
-        </div>
+        </>
     )
 }
