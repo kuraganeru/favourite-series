bb0acdbb7bff822a95bff7b9d272c8ec69000020
andy <n.andyheng@gmail.com>
2023-05-21 10:33:56 -0400

    update styles for AddSeriesSearch

--- src/components/AddSeriesSearch.jsx
+++ src/components/AddSeriesSearch.jsx
@@ -1,3 +1,5 @@
+import "./css/AddSeriesSearch.css"
+
 export default function AddSeriesSearch({ searchText, onSetSearchText, handleFetchData, requestLoading }) {
     function handleOnKeyDown(e) {
         if (e.key === "Enter") {
@@ -10,10 +12,11 @@ export default function AddSeriesSearch({ searchText, onSetSearchText, handleFet
             <input
                 type="text"
                 value={searchText}
+                className="search-input"
                 onChange={(e) => onSetSearchText(e.target.value)}
                 onKeyDown={(e) => handleOnKeyDown(e)}
             />
-            <button onClick={handleFetchData}>Click Me</button>
+            <button onClick={handleFetchData}>Search</button>
             <p>{requestLoading ? "Loading..." : "Waiting for request"}</p>
         </>
     )
