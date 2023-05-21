1eedb2c6c4393c1613059775019d9a31a27664ed
andy <n.andyheng@gmail.com>
2023-05-21 11:06:33 -0400

    update styles and component structure

--- src/components/SeriesCategoryHeader.jsx
+++ src/components/SeriesCategoryHeader.jsx
@@ -1,15 +1,18 @@
 import CategoryEdit from "./CategoryEdit"
+import "./css/SeriesCategoryHeader.css"
 
 export default function SeriesCategoryHeader({ categoryName, editCategoryName, onSetEditCategoryNameChange, onSetCategoryNameChange }) {
     return (
-        <>
-            {editCategoryName ?
-                <CategoryEdit categoryName={categoryName} onSetCategoryNameChange={onSetCategoryNameChange} /> :
-                <h2>{categoryName}</h2>
-            }
+        <header className="category-header">
+            <div className="header-title">
+                {editCategoryName ?
+                    <CategoryEdit categoryName={categoryName} onSetCategoryNameChange={onSetCategoryNameChange} /> :
+                    <h2>{categoryName}</h2>
+                }
+            </div>
             <button onClick={() => onSetEditCategoryNameChange(!editCategoryName)}>
                 {editCategoryName ? "Save" : "Edit"}
             </button>
-        </>
+        </header>
     )
 }
