ade82ac98dd5cf4dcdaeba755960e97f7457e418
andy <n.andyheng@gmail.com>
2023-05-27 15:06:09 -0400

    move onMouse events to parent container

--- src/components/SeriesItem.jsx
+++ src/components/SeriesItem.jsx
@@ -1,12 +1,13 @@
 export default function SeriesItem({ oneSeries, clickedSeries, onSetClickedSeriesChange, handleRemoveSeries }) {
     return (
-        <div>
-            <img 
-                className={`image ${clickedSeries?.originalElement !== "img" && clickedSeries?.id === oneSeries?.id ? "img-selected" : ""}`} 
-                src={`https://images.igdb.com/igdb/image/upload/t_thumb_2x/${oneSeries.cover.image_id}.jpg`} 
+        <div
+            onMouseEnter={() => onSetClickedSeriesChange(oneSeries, "img")}
+            onMouseLeave={() => onSetClickedSeriesChange(null)}
+        >
+            <img
+                className={`image ${clickedSeries?.originalElement !== "img" && clickedSeries?.id === oneSeries?.id ? "img-selected" : ""}`}
+                src={`https://images.igdb.com/igdb/image/upload/t_thumb_2x/${oneSeries.cover.image_id}.jpg`}
                 alt={oneSeries.title}
-                onMouseEnter={() => onSetClickedSeriesChange(oneSeries, "img")}
-                onMouseLeave={() => onSetClickedSeriesChange(null)}
             />
             <span onClick={() => handleRemoveSeries(oneSeries)}>✕</span>
         </div>
