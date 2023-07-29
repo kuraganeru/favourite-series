5c68cff8306ffeb26eb0cf7e0c08fef852c698be
andy <n.andyheng@gmail.com>
2023-07-29 14:41:52 -0400

    implement modal for settings

--- src/components/settings/Settings.jsx
+++ src/components/settings/Settings.jsx
@@ -1,5 +1,28 @@
-export default function Settings({bgColour, fontColour, setBgColour, setFontColour}) {
-    
+import { useState } from "react"
+import ReactModal from "react-modal"
+
+export default function Settings({ bgColour, fontColour, setBgColour, setFontColour }) {
+    const [modalIsOpen, setIsOpen] = useState(false);
+
+    const modalStyles = {
+        content: {
+            maxHeight: "400px",
+            left: "50%",
+            top: "50%",
+            transform: "translate(-50%, -50%)",
+            width: "25%",
+            padding: "40px"
+        }
+    }
+
+    function openModal() {
+        setIsOpen(true);
+    }
+
+    function closeModal() {
+        setIsOpen(false);
+    }
+
     function handleBgInput(e) {
         setBgColour(e.target.value)
     }
@@ -9,16 +32,26 @@ export default function Settings({bgColour, fontColour, setBgColour, setFontColo
     }
 
     return (
-        <div className="container">
-            <div className="setting">
-                <input type="color" name="color-bg" id="color-bg" onInput={handleBgInput} value={bgColour} />
-                <label htmlFor="color-bg">Background Colour</label>
-            </div>
+        <>
+            <button onClick={openModal}>Settings</button>
+            <ReactModal
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+                contentLabel="App Settings"
+                style={modalStyles}
+            >
+                <div className="container">
+                    <div className="setting">
+                        <input type="color" name="color-bg" id="color-bg" onInput={handleBgInput} value={bgColour} />
+                        <label htmlFor="color-bg">Background Colour</label>
+                    </div>
 
-            <div className="setting">
-                <input type="color" name="color-font" id="color-font" onInput={handleFontInput} value={fontColour} />
-                <label htmlFor="color-bg">Font Colour</label>
-            </div>
-        </div>
+                    <div className="setting">
+                        <input type="color" name="color-font" id="color-font" onInput={handleFontInput} value={fontColour} />
+                        <label htmlFor="color-bg">Font Colour</label>
+                    </div>
+                </div>
+            </ReactModal>
+        </>
     )
 }
